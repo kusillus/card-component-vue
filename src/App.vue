3b3839c8a0d93ee5f2d7b02ceeca94097d7e54df
Alexander Guevara <wguevarab7@gmail.com>
2018-04-29 21:44:33 -0500

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h3>
+            {{tittle}}
+        </h3>
+        <div class="container_cards">
+            <card-item
+                v-for="item in posts"
+                :title="item.title"
+                :content="item.content"
+                :img="item.img"
+                :id="item.id">
+            </card-item>
+        </div>
+    </div>
+</template>
+
+<script>
+import card from './components/cCardItem.vue'
+export default {
+    name: 'app',
+    data () {
+        return {
+            tittle: 'JCC POKÉMON',
+            posts: [
+                {
+                    id: 1,
+                    title: 'Silvally-GX',
+                    content: 'Este ataque hace 50 puntos de daño por cada Pokémon en Banca de tu rival. (No puedes usar más de 1 ataque GX en una partida).',
+                    img: 'SM4_ES_119.png',
+                },
+                {
+                    id: 2,
+                    title: 'Leafeon-GX',
+                    content: 'Cuando un Pokémon-GX queda Fuera de Combate, el rival toma 2 cartas de Premio.',
+                    img: 'SM5_ES_13.png',
+                },
+                {
+                    id: 3,
+                    title: 'Pikachu-EX',
+                    content: 'Descarta todas las Energías Lightning unidas a este Pokémon. Este ataque hace 50 puntos de daño por cada carta de Energía que hayas descartado',
+                    img: 'XYP_ES_XY124.png',
+                },
+                {
+                    id: 4,
+                    title: 'Piechu-GX',
+                    content: 'Cada vez que un Pikachu se encuentra con algo nuevo, le lanza una descarga eléctrica. Cuando se ve alguna baya chamuscada',
+                    img: 'XYP_ES_XY84.png',
+                },
+            ]
+        }
+    },
+    components:{
+        'card-item': card
+    }
+}
+</script>
+
+<style lang="scss">
+body{
+  background-color: #f4da00;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.container_cards{
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+}
+</style>
---
+++ src/components/cCardItem.vue
@@ -0,0 +1,100 @@
+<template>
+	<div :style="background_style"
+		class="card_container"
+		@click="onClick(id)">
+		<div class="card_content">
+			<div class="text_top">
+				{{title | limit(25)}}
+			</div>
+			<div class="text_bottom">
+				{{content | limit(80)}}
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['title', 'content', 'img', 'id'],
+	  	data () {
+	    	return {
+	      	background_style: {
+	      		background: 'linear-gradient(rgba(9, 0, 21, 0.7) 10%, rgba(0, 0, 0, 0) 20%, rgba(0, 0, 0, 0) 50%, rgba(18, 3, 39, 0.8) 80%), url("'+  require('../assets/'+ this.img) +'")',
+	      		backgroundSize: 'cover',
+				backgroundPosition: 'center'
+	      		}
+	    	}
+	  	},
+	  	methods: {
+	  		onClick: function (id) {
+	  			console.log('Pokemon id: ' + id)
+	  		}
+	  	},
+	  	filters: {
+	  		limit: function (text, length) {
+	  			console.log('filter')
+	  			let limitCount = parseInt(length, 10)
+	  			if(limitCount <= 0) {
+	  				return text
+	  			}
+	  			if(text.length > length) {
+	  				return text.slice(0, limitCount) + '...'	
+	  			} else {
+	  				return text	
+  				}
+  			}
+	  	}
+	}
+</script>
+<style lang="scss">
+ @import url('https://fonts.googleapis.com/css?family=Gugi');
+ @import url('https://fonts.googleapis.com/css?family=Exo+2');
+	.card_container{
+		width: 17rem;
+		height: 25rem;
+		margin: 0 .5rem 1rem 0;
+		background-size: cover;
+		background-position: center;
+		color: white;
+		border-radius: .8rem;
+		box-shadow: 0px 0px 15px 1px #0000004d;
+		cursor: pointer;
+		transition: all .3s ease;
+	}
+	.card_content{
+		text-align: left;
+		padding: 1rem;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		box-sizing: border-box;
+	}
+	.text_top{
+		font-weight: bold;
+		font-family: 'Gugi', cursive;
+	}
+	.text_bottom{
+		font-weight: 300;
+    	font-family: 'Exo 2', sans-serif;
+    	text-align: center;
+	}
+	.card_container:hover{
+		filter: brightness(140%);
+		// animation-name: changeLight;
+  //   	animation-duration: 1s;
+  //   	animation-iteration-count: infinite;
+	}
+	// Puedes habilitar el efecto descomentando las lienas anteriores
+	@keyframes changeLight {
+	    0% {filter: brightness(140%);}
+	    // 10% {filter: brightness(120%);}
+	    // 12% {filter: brightness(140%);}
+	    // 40% {filter: brightness(130%);}
+	    // 42% {filter: brightness(110%);}
+	    50% {filter: brightness(120%);}
+	    // 52% {filter: brightness(110%);}
+	    // 60% {filter: brightness(140%);}
+	    // 72% {filter: brightness(110%);}
+	    100% {filter: brightness(140%);}
+	}
+</style>
